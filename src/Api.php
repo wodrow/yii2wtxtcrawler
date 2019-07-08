dcbc3a2bebaba0513732709ff715d398e65c96a3
wodrow-ys-pc <wodrow451611cv@gmail.com>
2019-07-08 15:21:10 +0800

    init

---
+++ src/Api.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wodrow
+ * Date: 19-7-8
+ * Time: 下午3:18
+ */
+
+namespace wodrow\yii2wtxtcrawler;
+
+
+interface Api
+{
+
+}
---
+++ src/TxtCrawler.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wodrow
+ * Date: 19-7-8
+ * Time: 下午3:19
+ */
+
+namespace wodrow\yii2wtxtcrawler;
+
+
+class TxtCrawler
+{
+
+}
